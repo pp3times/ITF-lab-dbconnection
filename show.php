918b697156549325b5b44862c599a4908876f7e9
Jeremy <than2314@gmail.com>
2020-11-14 16:35:06 +0700

    ahah

--- show.php
+++ show.php
@@ -18,7 +18,8 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
+<table id="myTable" class="display" style="width: 100%;">
+
   <tr>
     <th width="100"> <div align="center">Name</div></th>
     <th width="350"> <div align="center">Comment </div></th>
